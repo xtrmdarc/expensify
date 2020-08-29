9ca3eb20914da235b21a10b86166cde11530b7ee
xtrmdarc <diegoreyesco@hotmail.com>
2020-08-29 12:43:18 -0500

    Add test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'should be invalid if no password is provided' do
-      
+    user = User.new username: 'admin'
+    expect(user.valid?).to eq(false)
   end
 
+  it 'should be invalid if username has already been taken' do 
+    User.create username: 'admin', password: '123', password_confirmation: '123'
+    user = User.new username: 'admin', password: '1232', password_confirmation: '1232'
+    expect(user.valid?).to eq(false)
+  end
 
+  it 'should be valid ' do 
+    user = User.new username: 'test', password: '123', password_confirmation: '123'
+    user.save!
+    expect(user.valid?).to eq(true)
+  end
 end
